b992cf6ea4bde67dfdd304332ea9b412deefb267
Rob Jahn <robjahn@gmail.com>
2019-04-21 08:54:45 -0400

    update to 30 sec and add prompt to continue

--- 6-onboardOrderApp.sh
+++ 6-onboardOrderApp.sh
@@ -5,7 +5,7 @@ exec > >(tee -i $LOG_LOCATION/6-onboardOrderApp.log)
 exec 2>&1
 
 clear
-
+echo "Gathering keptn-api-token and keptn endpoint..." 
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -o=yaml | yq - r data.keptn-api-token | base64 --decode)
 KEPTN_ENDPOINT=https://$(kubectl get ksvc -n keptn control -o=yaml | yq r - status.domain)
 GITHUB_PERSONAL_ACCESS_TOKEN=$(cat creds.json | jq -r '.githubPersonalAccessToken')
@@ -14,16 +14,17 @@ GITHUB_ORGANIZATION=$(cat creds.json | jq -r '.githubOrg')
 KEPTN_PROJECT=orders-project
 
 echo "-----------------------------------------------------"
-echo "Configuring keptn CLI with:"
+echo "About to configure keptn CLI with:"
 echo "KEPTN endpoint               : $KEPTN_ENDPOINT"
 echo "KEPTN API token              : $KEPTN_API_TOKEN"
 echo "GitHub User Name             : $GITHUB_USER_NAME"
 echo "GitHub Personal Access Token : $GITHUB_PERSONAL_ACCESS_TOKEN"
 echo "GitHub Organization          : $GITHUB_ORGANIZATION" 
 echo ""
-echo "*** NOTE: This will first delete the keptn project "
-echo "          in the the target github organization ***"
+echo "*** NOTE: This will first delete the keptn project repo:"
+echo "          http://www.github.com/$GITHUB_ORGANIZATION/$KEPTN_PROJECT ***"
 echo "-----------------------------------------------------"
+read -rsp $'Press ctrl-c to abort. Press any key to continue...\n' -n1 key
 echo ""
 echo "-----------------------------------------------------"
 echo "Running 'deleting project $KEPTN_PROJECT ' "
@@ -40,27 +41,32 @@ echo ""
 echo "Sleeping 30 sec to allow project to be registered"
 sleep 30
 echo ""
+
 echo "-----------------------------------------------------"
 echo "Running 'keptn onboard service'"
 echo "-----------------------------------------------------"
 echo "front-end"
 keptn onboard service --project=$KEPTN_PROJECT --values=./keptn-onboarding/values_front-end.yaml
 echo ""
-echo "Sleeping 10 sec to allow for service to be registered"
-sleep 10
+echo "Sleeping 30 sec to allow for service to be registered"
+sleep 30
+
 echo "customer-service"
 keptn onboard service --project=$KEPTN_PROJECT --values=./keptn-onboarding/values_customer-service.yaml
 echo ""
-echo "Sleeping 10 sec to allow for service to be registered"
-sleep 10
+echo "Sleeping 30 sec to allow for service to be registered"
+sleep 30
+
 echo "order-service"
 keptn onboard service --project=$KEPTN_PROJECT --values=./keptn-onboarding/values_order-service.yaml
 echo ""
-echo "Sleeping 10 sec to allow for service to be registered"
-sleep 10
+echo "Sleeping 30 sec to allow for service to be registered"
+sleep 30
+
 echo "catalog-service"
 keptn onboard service --project=$KEPTN_PROJECT --values=./keptn-onboarding/values_catalog-service.yaml
-
+echo "Sleeping 30 sec to allow for service to be registered"
+sleep 30
 
 echo ""
 echo "-----------------------------------------------------"
