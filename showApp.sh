8bd0e3128a5fdcce807d9d789d1c68cf8bc8118c
Rob Jahn <robjahn@gmail.com>
2019-04-21 08:55:19 -0400

    Update to use ingress IP

--- showApp.sh
+++ showApp.sh
@@ -4,25 +4,27 @@ LOG_LOCATION=./logs
 exec > >(tee -i $LOG_LOCATION/showApp.log)
 exec 2>&1
 
-export DEV_IP=$(kubectl -n dev get svc front-end -o json | jq -r '.status.loadBalancer.ingress[0].ip')
 export INGRESS_IP=$(kubectl get svc istio-ingressgateway -n istio-system -o=json | jq -r .status.loadBalancer.ingress[].ip)
 
 echo ""
 echo "--------------------------------------------------------------------------"
+echo "kubectl -n dev get svc"
+kubectl -n dev get svc
+echo "--------------------------------------------------------------------------"
 echo "Orders Application:"
 echo "--------------------------------------------------------------------------"
-echo "Dev running        @ http://front-end.dev.$DEV_IP.xip.io/"
+echo "Dev running        @ http://front-end.dev.$INGRESS_IP.xip.io/"
 echo "Staging running    @ http://front-end.staging.$INGRESS_IP.xip.io/"
 echo "Production running @ http://front-end.production.$INGRESS_IP.xip.io/"
 cho "--------------------------------------------------------------------------"
 echo ""
 echo "--------------------------------------------------------------------------"
-echo "Kubernetes dev pods"
+echo "kubectl get pods -n dev"
 kubectl get pods -n dev
 echo ""
 echo "--------------------------------------------------------------------------"
-echo "Kubernetes staging pods"
+echo "kubectl get pods -n staging"
 kubectl get pods -n staging
 echo "--------------------------------------------------------------------------"
-echo "Kubernetes production pods"
+echo "kubectl get pods -n production"
 kubectl get pods -n production
