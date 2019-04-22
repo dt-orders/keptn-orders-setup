d3e275357456af58cd943820ea3afecedae6068b
rob jahn <robjahn@gmail.com>
2019-04-22 19:05:49 -0400

    add echo output and option to abort

--- configureGcloudGke.sh
+++ configureGcloudGke.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-LOG_LOCATION=./logs
-exec > >(tee -i $LOG_LOCATION/connectCluster.log)
-exec 2>&1
-
 clear
-echo "Configuring gcloud connection with values in creds.json"
-export CLUSTER_NAME=$(cat creds.json | jq -r '.clusterName')
-export CLUSTER_ZONE=$(cat creds.json | jq -r '.clusterZone')
-export CLUSTER_REGION=$(cat creds.json | jq -r '.clusterRegion')
-export GKE_PROJECT=$(cat creds.json | jq -r '.gkeProject')
+CLUSTER_NAME=$(cat creds.json | jq -r '.clusterName')
+CLUSTER_ZONE=$(cat creds.json | jq -r '.clusterZone')
+CLUSTER_REGION=$(cat creds.json | jq -r '.clusterRegion')
+GKE_PROJECT=$(cat creds.json | jq -r '.gkeProject')
+
+echo "Run these commands on your laptop to configure and run kinbana"
+echo ""
+echo "gcloud --quiet config set project $GKE_PROJECT"
+echo "gcloud --quiet config set container/cluster $CLUSTER_NAME"
+echo "gcloud --quiet config set compute/zone $CLUSTER_ZONE"
+echo "gcloud container clusters get-credentials $CLUSTER_NAME --zone $CLUSTER_ZONE --project $GKE_PROJECT"
+echo ""
+echo "Would you like me to run them now?"
+read -rsp $'Press ctrl-c to abort. Press any key to continue...\n' -n1 key
 
 gcloud --quiet config set project $GKE_PROJECT
 gcloud --quiet config set container/cluster $CLUSTER_NAME
