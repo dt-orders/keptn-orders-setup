746adb42c415eea5a6cfcf944e12cabbc4d33627
rob jahn <robjahn@gmail.com>
2019-07-25 10:16:12 -0500

    adjust for kubectl commands for 040

--- showKeptn.sh
+++ showKeptn.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export BROKER_DOMAIN_URL=$(kubectl get ksvc event-broker-ext -n keptn -o=json | jq -r .status.domain)
-export KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -o=yaml | yq - r data.keptn-api-token | base64 --decode)
+clear
 
-echo ""
 echo "--------------------------------------------------------------------------"
 echo "kubectl -n keptn get pods"
 echo "--------------------------------------------------------------------------"
@@ -20,19 +18,9 @@ kubectl get svc istio-ingressgateway -n istio-system
 echo "--------------------------------------------------------------------------"
 echo ""
 echo "--------------------------------------------------------------------------"
-echo "kubectl get routes -n keptn"
-kubectl get routes -n keptn
+echo "kubectl -n keptn get svc"
+kubectl -n keptn get svc 
 echo "--------------------------------------------------------------------------"
-echo ""
-echo "--------------------------------------------------------------------------"
-echo "kubectl get channels -n keptn"
-kubectl get channels -n keptn
-echo "--------------------------------------------------------------------------"
-echo ""
-echo "--------------------------------------------------------------------------"
-echo "kubectl get subscription -n keptn"
-echo "--------------------------------------------------------------------------"
-kubectl get subscription -n keptn
 echo ""
 echo "--------------------------------------------------------------------------"
 echo "kubectl get pods -n istio-system"
@@ -40,14 +28,12 @@ kubectl get pods -n istio-system
 echo "--------------------------------------------------------------------------"
 echo ""
 echo "--------------------------------------------------------------------------"
-echo "kubectl get pods -n knative-serving"
-kubectl get pods -n knative-serving
+echo "kubectl -n keptn get deployments"
+kubectl -n keptn get deployments
 echo "--------------------------------------------------------------------------"
 echo ""
-# Retrieve keptn endpoint and api-token
-KEPTN_ENDPOINT=https://$(kubectl get ksvc -n keptn control -o=yaml | yq r - status.domain)
-KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -o=yaml | yq - r data.keptn-api-token | base64 --decode)
-
-echo "keptn endpoint: $KEPTN_ENDPOINT"
-echo "keptn api-token: $KEPTN_API_TOKEN"
+echo "--------------------------------------------------------------------------"
+echo "kubectl get ns"
+kubectl get ns
+echo "--------------------------------------------------------------------------"
 echo ""
